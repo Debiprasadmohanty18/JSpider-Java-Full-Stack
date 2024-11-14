5045914717204a72c45d03bfb119a366e8fd9af4
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-11-14 23:05:53 +0530

    Add files via upload

---
+++ Pattern/HallowSquare.java
@@ -0,0 +1,47 @@
+
+
+public class HallowSquare {
+    public static void main(String[] args) {
+        
+        int n = 5;
+
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print("X");
+        }
+        System.out.println();
+        for(int i=1;i<=n-2;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(j == 1 || j == n)
+                    System.out.print("X");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            System.out.print("X");
+        }
+
+        System.out.println();
+
+
+// 2nd Approach
+
+        // for(int i=1;i<=n;i++)
+        // {
+        //     for(int j=1;j<=n;j++)
+        //     {
+        //         if(i==1 || i==n || j == 1 || j == n)
+        //             System.out.print("X");
+        //         else
+        //             System.out.print(" ");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
