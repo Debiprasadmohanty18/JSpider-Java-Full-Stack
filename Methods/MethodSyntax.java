89fd1d0dc043e36bf676b5f222d9153abae8e16b
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-11-18 20:57:52 +0530

    Add files via upload

---
+++ Methods/MethodSyntax.java
@@ -0,0 +1,23 @@
+public class MethodSyntax {
+
+    public static void m1(int i)
+    {
+        //Body...    
+    }
+
+    /*
+        void m1() --> Method Signature
+
+        public static void m1() --> Method Declaration
+        {
+                ---> Method Body
+        }
+
+        m1(); --> Method Calling
+
+        m1(int i,int j);  ---> int i, int j are Formal Arguments
+        
+        m1(5,6); --> 5,6 are Actual Arguments
+
+     */
+}
