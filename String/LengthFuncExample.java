1270e8d56cc00e0b59bb9b99dcb1196cdeb54dd2
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-06 11:34:50 +0530

    Add files via upload

---
+++ String/LengthFuncExample.java
@@ -0,0 +1,14 @@
+package com.String;
+
+public class LengthFuncExample {
+
+	public static void main(String[] args) {
+		
+		String s = "Debiprasad";
+		
+		int n = s.length();
+		System.out.println("Length of " + "\"" + s + "\"" + " is : " + n);
+
+	}
+
+}
