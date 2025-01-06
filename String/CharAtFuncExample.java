a930fc6ce01417d591c3946ca863fad3ac890d70
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-06 19:40:21 +0530

    Add files via upload

---
+++ String/CharAtFuncExample.java
@@ -0,0 +1,14 @@
+package com.String;
+
+public class CharAtFuncExample {
+	public static void main(String[] args) {
+		
+		String s = "Debiprasad";
+		
+		for(int i=0;i<s.length();i++)
+		{
+			System.out.println("Position : " + i + ", Character : " + s.charAt(i));
+		}
+		
+	}
+}
