dc772e5da117c333ea03ecfe0bcb0d94df576b60
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-07 19:38:41 +0530

    Add files via upload

---
+++ String/ExampleOftoCharArrayFunc.java
@@ -0,0 +1,16 @@
+package com.String;
+
+public class ExampleOftoCharArrayFunc {
+
+	public static void main(String[] args) {
+		
+		String s1 = "Debiprasad";
+		
+		char[] ch = s1.toCharArray();
+		
+		for(char res : ch)
+			System.out.println(res);
+
+	}
+
+}
