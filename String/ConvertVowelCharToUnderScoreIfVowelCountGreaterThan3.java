b73ca06f75d747655e26b304a7edc4ea44b1b929
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-07 10:20:46 +0530

    Update ConvertVowelCharToUnderScoreIfVowelCountGreaterThan3.java

--- String/ConvertVowelCharToUnderScoreIfVowelCountGreaterThan3.java
+++ String/ConvertVowelCharToUnderScoreIfVowelCountGreaterThan3.java
@@ -1,10 +1,18 @@
 package com.String;
 
+/*
+ 
+ 1 - input :- "I Love Java"    ,   output :- "_ L_v_ J_v_"
+ 2 - input :- "Java"    ,   output :- "Java"
+ 3 - input :- "Javvaaa"    ,   output :- "J_vv___"
+ 
+*/
+
 public class ConvertVowelCharToUnderScoreIfVowelCountGreaterThan3 {
 
 	public static void main(String[] args) {
 		
-		String s = "  Java  ";
+		String s = "I Love Java";
 		char[] ch = s.toCharArray();
 		int vowelCount = 0;
 		for(int i=0;i<ch.length;i++)
