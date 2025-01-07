77397e0553248625b19e55e0b2bee539f4c991ba
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2025-01-07 10:19:47 +0530

    Update ChangeConsecutiveCharacter.java

--- String/ChangeConsecutiveCharacter.java
+++ String/ChangeConsecutiveCharacter.java
@@ -10,7 +10,7 @@ package com.String;
 public class ChangeConsecutiveCharacter {
 
 	public static void main(String[] args) {
-		String s = "BOSSSSSs";
+		String s = "BOSSSS";
 		char[] ch = s.toCharArray();
 		
 		for(int i=0;i<ch.length-1;i++)
